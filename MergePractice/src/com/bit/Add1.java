32ab9920826c08f2aa73f15fc80b6fa9305da1c0
santhosh <santhosh@SanthoshNaroju>
2019-04-02 17:25:30 +0530

    commited

--- MergePractice/src/com/bit/Add1.java
+++ MergePractice/src/com/bit/Add1.java
@@ -4,5 +4,6 @@ public class Add1 {
 public static void main(String[] args) {
 System.out.println("hiiii everyone");
 System.out.println("hello model1 from developer1");
+System.out.println("santhosh");
 }
 }
