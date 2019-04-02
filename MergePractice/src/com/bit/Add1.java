aeda8d1db884b5ff59b609f8d117e6a010ebf423
santhosh <santhosh@SanthoshNaroju>
2019-04-02 16:47:01 +0530

    new branch added

--- MergePractice/src/com/bit/Add1.java
+++ MergePractice/src/com/bit/Add1.java
@@ -2,6 +2,7 @@ package com.bit;
 
 public class Add1 {
 public static void main(String[] args) {
-System.out.println("hiiii everyone");	
+System.out.println("hiiii everyone");
+System.out.println("hello model1 from developer1");
 }
 }
